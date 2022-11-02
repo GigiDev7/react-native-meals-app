d6c4a9d19cdaf69271b9805280ee9f5749f0032a
GigiDev7 <gigivadachkoria@gmail.com>
2022-11-02 14:45:12 +0400

    added styling for meal items

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,11 +1,78 @@
-import { Text, View } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Pressable,
+  Image,
+  Platform,
+} from "react-native";
 
 const MealItem = ({ meal }) => {
   return (
-    <View>
-      <Text>{meal.title}</Text>
+    <View style={styles.mealItem}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => (pressed ? styles.pressed : null)}
+      >
+        <View style={styles.innerContainer}>
+          <View>
+            <Image style={styles.img} source={{ uri: meal.imageUrl }} />
+            <Text style={styles.title}>{meal.title}</Text>
+          </View>
+          <View style={styles.details}>
+            <Text style={styles.detailItem}>{meal.duration}m</Text>
+            <Text style={styles.detailItem}>
+              {meal.complexity.toUpperCase()}
+            </Text>
+            <Text style={styles.detailItem}>
+              {meal.affordability.toUpperCase()}
+            </Text>
+          </View>
+        </View>
+      </Pressable>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  mealItem: {
+    margin: 16,
+    borderRadius: 8,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+    backgroundColor: "white",
+    elevation: 4,
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    shadowOffset: { width: 0, height: 2 },
+  },
+  innerContainer: {
+    borderRadius: 8,
+    overflow: "hidden",
+  },
+  img: {
+    width: "100%",
+    height: 200,
+  },
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+    margin: 8,
+  },
+  details: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 8,
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 13,
+  },
+  pressed: {
+    opacity: 0.5,
+  },
+});
 
 export default MealItem;
